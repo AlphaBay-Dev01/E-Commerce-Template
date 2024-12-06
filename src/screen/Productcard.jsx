1f9b0b13984c46365710d32d2cead4fb3da3b937
Sodadoung <sodadoung@gmail.com>
2024-12-06 11:18:24 +0700

    update

---
+++ src/screen/Productcard.jsx
@@ -0,0 +1,14 @@
+﻿
+import React from 'react'
+
+const Productcard = () => {
+  return (
+    <div>
+        <h1>Productcard</h1>
+         <div ></div>
+      
+    </div>
+  )
+}
+
+export default Productcard
